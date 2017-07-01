8df660d1327b50acb5b59292c2e6a3abd5618747
L-47 <thelordofblackops2@gmail.com>
2017-07-01 20:22:56 +0200

    added random code

--- jema.go
+++ jema.go
@@ -1,9 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/fatih/color"
+)
 
 func main() {
 	// comment?
+	notice := color.New(color.Bold, color.FgGreen).PrintlnFunc()
+	notice("testing...")
 	fmt.Printf("decimal: %d\nbinary: %b\noctal: %#o\nhex: %#x\n", 42, 42, 42, 42)
-	fmt.Println("")
+
 }
