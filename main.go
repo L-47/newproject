fbe59f0c2596f891ba1fb1b9d724c565924a7d6f
L-47 <thelordofblackops2@gmail.com>
2017-07-05 20:38:07 +0200

    changed name to main.go

--- main.go
+++ main.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 	"github.com/fatih/color"
 )
-
 func main() {
 	// comment?
+	var c bool
+	c = true
 	notice := color.New(color.Bold, color.FgGreen).PrintlnFunc()
 	notice("testing...")
 	fmt.Printf("decimal: %d\nbinary: %b\noctal: %#o\nhex: %#x\n", 42, 42, 42, 42)
+	fmt.Printf("do computers love Dr albion Morozov? : %t",c)
 
 }
